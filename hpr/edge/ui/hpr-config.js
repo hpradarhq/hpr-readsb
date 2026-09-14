5e2e3ff49f3ed2f62c2cec9202caed308d6bea28
Nguyen Trong Thanh <59362196+ngtrthanh@users.noreply.github.com>
2026-09-15 06:42:33 +0700

    feat(edge): load detail hierarchy [skip ci]

--- hpr/edge/ui/hpr-config.js
+++ hpr/edge/ui/hpr-config.js
@@ -7,3 +7,4 @@ document.write('<link rel="stylesheet" href="/edge-g1-list.css">');
 document.write('<script src="/edge-g3-map.js"><\/script>');
 document.write('<script src="/edge-g4-calm-list.js"><\/script>');
 document.write('<script src="/edge-g5-detail.js"><\/script>');
+document.write('<script src="/edge-g6-detail-layout.js"><\/script>');
