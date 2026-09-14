0ae24c49bd7ebae173a4b3d2bcb0f050ee209473
Nguyen Trong Thanh <59362196+ngtrthanh@users.noreply.github.com>
2026-09-15 06:40:35 +0700

    feat(edge): load synchronous detail identity [skip ci]

--- hpr/edge/ui/hpr-config.js
+++ hpr/edge/ui/hpr-config.js
@@ -6,3 +6,4 @@ window.HPR_CONFIG = {
 document.write('<link rel="stylesheet" href="/edge-g1-list.css">');
 document.write('<script src="/edge-g3-map.js"><\/script>');
 document.write('<script src="/edge-g4-calm-list.js"><\/script>');
+document.write('<script src="/edge-g5-detail.js"><\/script>');
