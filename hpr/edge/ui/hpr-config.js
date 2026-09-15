b309c0bf78b5d583c1898464eb5752ab0c064d41
Nguyen Trong Thanh <59362196+ngtrthanh@users.noreply.github.com>
2026-09-15 16:18:24 +0700

    feat(edge): load Vietnam label policy [skip ci]

--- hpr/edge/ui/hpr-config.js
+++ hpr/edge/ui/hpr-config.js
@@ -19,3 +19,4 @@ document.write('<script src="/edge-g13-receiver.js"><\/script>');
 document.write('<script src="/edge-g14-display.js"><\/script>');
 document.write('<script src="/edge-g15-feeds.js"><\/script>');
 document.write('<script src="/edge-g16-security.js"><\/script>');
+document.write('<script src="/edge-g17-vn-labels.js"><\/script>');
